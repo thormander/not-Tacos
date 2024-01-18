0908aba92794ed7e5ece947c74ced0785ef65dac
thormander <71967190+thormander@users.noreply.github.com>
2024-01-18 17:53:51 -0600

    DesignComputerController updated

--- src/main/java/nottaco/web/DesignComputerController.java
+++ src/main/java/nottaco/web/DesignComputerController.java
@@ -3,6 +3,10 @@ package nottaco.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,7 @@ import nottaco.Hardware;
 import nottaco.Hardware.Type;
 import nottaco.Computer;
 import nottaco.ComputerOrder;
+import nottaco.data.HardwareRepository;
 
 import jakarta.validation.Valid; // note: javax changed to jakarta
 import org.springframework.validation.Errors;
@@ -25,27 +30,22 @@ import org.springframework.validation.Errors;
 @RequestMapping("/design")
 @SessionAttributes("computerOrder")
 public class DesignComputerController {
+  
+  private final HardwareRepository hardwareRepo;
+  
+  @Autowired
+  public DesignComputerController(
+    HardwareRepository hardwareRepo) {
+      this.hardwareRepo = hardwareRepo;
+  }
 
-@ModelAttribute
-public void addHardwaresToModel(Model model) {
-	List<Hardware> Hardwares = Arrays.asList(
-	  new Hardware("MATX", "mATX", Type.CASE),
-	  new Hardware("IITX", "ITX", Type.CASE),
-	  new Hardware("INTL", "Intel", Type.CPU),
-	  new Hardware("CAMD", "AMD", Type.CPU),
-	  new Hardware("NVDA", "Nvidia", Type.GPU),
-	  new Hardware("GAMD", "AMD", Type.GPU),
-	  new Hardware("NORM", "1 TB", Type.STORAGE),
-	  new Hardware("CRZY", "100 TB", Type.STORAGE),
-	  new Hardware("AAIR", "Air", Type.COOLING),
-	  new Hardware("WATR", "Water", Type.COOLING)
-	);
-
-	Type[] types = Hardware.Type.values();
-	for (Type type : types) {
-	  model.addAttribute(type.toString().toLowerCase(),
-	      filterByType(Hardwares, type));
-	}
+  @ModelAttribute
+  public void addIngredientsToModel(Model model) {
+    Iterable<Hardware> hardwares = hardwareRepo.findAll();
+    Type[] types = Hardware.Type.values();
+    for (Type type : types) {
+      model.addAttribute(type.toString().toLowerCase(), filterByType(hardwares, type));
+    }
   }
 
   @ModelAttribute(name = "computerOrder")
@@ -63,38 +63,22 @@ public void addHardwaresToModel(Model model) {
     return "design";
   }
 
-/*
   @PostMapping
-  public String processComputer(Computer taco,
-  			@ModelAttribute ComputerOrder tacoOrder) {
-    tacoOrder.addComputer(taco);
-    log.info("Processing taco: {}", taco);
+  public String processComputer(@Valid Computer computer, Errors errors, @ModelAttribute ComputerOrder computerOrder) {
+    if (errors.hasErrors()) {
+      return "design";
+    }
+
+    computerOrder.addComputer(computer);
+    log.info("Processing computer: {}", computer);
 
     return "redirect:/orders/current";
   }
- */
 
- @PostMapping
- public String processComputer(
-         @Valid Computer computer, Errors errors,
-         @ModelAttribute ComputerOrder computerOrder) {
-
-   if (errors.hasErrors()) {
-     return "design";
-   }
-
-   computerOrder.addComputer(computer);
-   log.info("Processing computer: {}", computer);
-
-   return "redirect:/orders/current";
- }
-
- private Iterable<Hardware> filterByType(
-     List<Hardware> hardwares, Type type) {
-   return hardwares
-             .stream()
-             .filter(x -> x.getType().equals(type))
-             .collect(Collectors.toList());
- }
+  private Iterable<Hardware> filterByType(Iterable<Hardware> hardwares, Type type) {
+    return StreamSupport.stream(hardwares.spliterator(), false)
+              .filter(i -> i.getType().equals(type))
+              .collect(Collectors.toList());
+  }
 
 }

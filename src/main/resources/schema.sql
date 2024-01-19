88e961bbbc421fee95343b8a8bfe1f4f81578c09
thormander <71967190+thormander@users.noreply.github.com>
2024-01-18 18:17:46 -0600

    SQL files added and updated

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,39 @@
+create table if not exists Computer_Order (
+  id identity,
+  delivery_Name varchar(50) not null,
+  delivery_Street varchar(50) not null,
+  delivery_City varchar(50) not null,
+  delivery_State varchar(2) not null,
+  delivery_Zip varchar(10) not null,
+  cc_number varchar(16) not null,
+  cc_expiration varchar(5) not null,
+  cc_cvv varchar(3) not null,
+  placed_at timestamp not null
+);
+
+create table if not exists Computer (
+  id identity,
+  name varchar(50) not null,
+  computer_order bigint not null,
+  computer_order_key bigint not null,
+  created_at timestamp not null
+);
+
+create table if not exists Hardware_Ref (
+  hardware varchar(4) not null,
+  computer bigint not null,
+  computer_key bigint not null
+);
+
+
+create table if not exists Hardware (
+  id varchar(4) not null,
+  name varchar(25) not null,
+  type varchar(10) not null
+);
+
+
+alter table Computer
+    add foreign key (computer_order) references Computer_Order(id);
+alter table Hardware_Ref
+    add foreign key (hardware) references Hardware(id);
